20398011ab1b8b362d32eb16bb4b12eaad634e56
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2018-10-06 13:19:39 -0400

    Added form for submitting user data

--- Matcher/views.py
+++ Matcher/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+from .forms import MatcherForm
+
 
 def get_match(request):
-	return render(request, template_name="matcher.html")
+	if request.method == "POST":
+		data = MatcherForm(request.POST)
+		if data.is_valid():
+			gpa = data.cleaned_data['unweighted_gpa']
+			sat = data.cleaned_data['sat_score']
+			act = data.cleaned_data['act_score']
+			print(gpa, sat, act)
+			# if not sat and not act:
+			# 	raise ValidationError("SAT or ACT must be used")
+
+			return render(request, "results.html", {"data": data})
+		else:
+			return render(request, "matcher.html", {"data": data})
+
+
+
+	else:
+		data = MatcherForm()
+		return render(request, "matcher.html", {"data": data})
