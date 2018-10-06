d98842a3f700636ead730efac4ef9a4784851c8c
Shilong Dai <sdai@viperfish.net>
2018-10-06 15:43:18 -0400

    added distribution estimation

--- CollegeFinder/urls.py
+++ CollegeFinder/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('matcher/', include("Matcher.urls")),
-	path('', RedirectView.as_view(url="/matcher/"))
+	path('', RedirectView.as_view(url = "/matcher/"))
 ]
---
+++ Matcher/statistics/distribution.py
@@ -0,0 +1,25 @@
+from scipy.stats import norm
+
+
+class EstimatedNormal:
+	fifty_percent_z_score = 0.67448
+
+	def __init__(self, mean, iqr_low, iqr_high):
+		self.mean = mean
+		self.iqr_low = iqr_low
+		self.iqr_high = iqr_high
+		self.stdev = self.estimated_standard_dev()
+
+	def estimated_standard_dev(self):
+		value_difference = self.iqr_high - self.mean
+		return value_difference / EstimatedNormal.fifty_percent_z_score
+
+	def calculate_z_score(self, test_score):
+		difference_from_mean = test_score - self.mean
+		return difference_from_mean - self.stdev
+
+	def cdf(self, test):
+		return norm.cdf(self.calculate_z_score(test))
+
+	def pdf(self, test):
+		return norm.pdf(self.calculate_z_score(test))
