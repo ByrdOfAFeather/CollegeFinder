f65cb1e9d0c871ea9837c5fc4beadfb51f37953e
Shilong Dai <sdai@viperfish.net>
2018-10-07 00:28:21 -0400

    fixed distribution

--- Matcher/statistics/distribution.py
+++ Matcher/statistics/distribution.py
@@ -1,3 +1,5 @@
+import unittest
+
 from scipy.stats import norm
 
 
@@ -16,10 +18,21 @@ class EstimatedNormal:
 
 	def calculate_z_score(self, test_score):
 		difference_from_mean = test_score - self.mean
-		return difference_from_mean - self.stdev
+		return difference_from_mean / self.stdev
 
 	def cdf(self, test):
 		return norm.cdf(self.calculate_z_score(test))
 
 	def pdf(self, test):
 		return norm.pdf(self.calculate_z_score(test))
+
+
+class TestDistribution(unittest.TestCase):
+
+	def testCDF(self):
+		estimatedNormal = EstimatedNormal(20, 18, 22)
+		self.assertEqual(0.5, estimatedNormal.cdf(20))
+
+
+if __name__ == "__main__":
+	unittest.main()
